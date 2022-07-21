9c01540dcf6c1194c60af4f2cf20b55ab3a1d067
Yogesh Jadhav <52121757+yogeshjadhav1997@users.noreply.github.com>
2022-07-21 12:41:16 +0530

    Update Covid19_Project.sql

--- Covid19_Project.sql
+++ Covid19_Project.sql
@@ -1,3 +1,9 @@
+/*
+Covid 19 Data Exploration 
+Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
+*/
+
+
 select *
 from PortFolioProject..CovidDeaths
 where continent is not null
